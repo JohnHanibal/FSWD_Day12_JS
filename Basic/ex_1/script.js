13791fcb30f23d3c5028aef3522c4753331e5a6b
JohnHanibal <Johannes_aho@outlook.com>
2022-01-27 10:03:50 +0100

    ex 1 done

---
+++ Basic/ex_1/script.js
@@ -0,0 +1,6 @@
+function cristalGazer(job_titel,location,partner_name,number_childrens) {
+
+    return ("You will be a " +job_titel+ " in " +location+ " and married to "+partner_name+ " with " + number_childrens)
+}
+
+console.log(cristalGazer("Doctor", "Russia", "Thomas", "fur babys."));
