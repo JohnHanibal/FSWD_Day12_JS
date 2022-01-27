d63b13bef3ff11fa48ca42561cadbf521a874883
JohnHanibal <Johannes_aho@outlook.com>
2022-01-27 16:00:27 +0100

    end

--- Challenge/script.js
+++ Challenge/script.js
@@ -1,10 +1,10 @@
 function atm(amt){
     quotient = parseInt(amt/100);
     quotient2 = parseInt((amt-100*quotient)/50);
-    quotient3 = parseInt((amt-100*quotient-50*quotient2)/20);
+    quotient3 =parseInt((amt-100*quotient-50*quotient2)/20);
     quotient4= parseInt((amt-100*quotient-50*quotient2-20*quotient3)/10);
 
-    return (amt + " = " + "<br>" + quotient+" note(s) of 100 Euro " + "<br>" + quotient2 + " note(s) of 50 Euro " + "<br>" + quotient3 + " note(s) of 20 Euro " + "<br>" + quotient4 + " note(s) of 10 Euros ");
+    return (quotient+" notes of 100 Euro " +"\n"+ quotient2 + " notes of 50 Euro " +quotient3 +" notes of 20 Euro " +quotient4 +" notes of 10 Euros ");
 }
 
-document.write(atm(1345));
+console.log(atm(280));
