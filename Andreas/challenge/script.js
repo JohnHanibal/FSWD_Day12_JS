7e5891b0ae284e8157e992a72eaf27e30b66fcd5
AndreasPlangger <andreas_plangger@hotmail.com>
2022-01-27 21:54:07 +0100

    .

--- Andreas/challenge/script.js
+++ Andreas/challenge/script.js
@@ -1,6 +1,7 @@
 function atm (amount){
 
     var $100=Math.floor(amount/100)
+    
     var rest =minutes%60
 
     return "You will receive "+$100+" 100€"+"\n"+$50+" 50€"+"\n"+$20+" 20€ and "+"\n"+$10+"10€ bills now"
