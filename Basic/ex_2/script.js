81a47c33f6a630bca7e3552c955f951d5179ef66
KateKatze <dr.katekogler@gmail.com>
2022-01-27 10:09:46 +0100

    ex 2 done

---
+++ Basic/ex_2/script.js
@@ -0,0 +1,8 @@
+const ageCalculator = (birthYear, currentYear) => {
+ return currentYear - birthYear;
+};
+
+const currentDate = new Date ();
+
+console.log(ageCalculator(1993, currentDate.getFullYear()));
+
