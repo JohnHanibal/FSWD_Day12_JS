59918ec34e16abdeac9af4da7e449ae300559f9c
KateKatze <dr.katekogler@gmail.com>
2022-01-27 11:46:25 +0100

    advanced done!

---
+++ Advanced/script.js
@@ -0,0 +1,7 @@
+function timeConv (min) {
+    hrs = parseInt(min /60);
+    rest=min%60;
+    return (min + " minutes" + " is " + hrs + " hour(s) and " + rest + " minutes.");
+}
+
+console.log(timeConv(186));
