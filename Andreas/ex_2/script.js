a224414aafc5de497aa5f9837e927209d5241d94
AndreasPlangger <andreas_plangger@hotmail.com>
2022-01-27 11:07:37 +0100

    add alt age ex_2

--- Andreas/ex_2/script.js
+++ Andreas/ex_2/script.js
@@ -1,5 +1,9 @@
 function ageCalculator(birth_year,current_year){
     return current_year-birth_year
 }
+    var yourAge=ageCalculator(1993,2022);
+    
+    var altAge=(yourAge-1);
+   
 
-console.log(ageCalculator(1993,2022));
+    console.log("Your are either "+yourAge+" or " +altAge+".");
