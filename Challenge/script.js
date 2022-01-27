d68351084aede2b3d202fa63547c10bcfb144ae3
KateKatze <dr.katekogler@gmail.com>
2022-01-27 12:43:22 +0100

    all fixed

--- Challenge/script.js
+++ Challenge/script.js
@@ -4,7 +4,7 @@ function atm(amt){
     quotient3 =parseInt((amt-100*quotient-50*quotient2)/20);
     quotient4= parseInt((amt-100*quotient-50*quotient2-20*quotient3)/10);
 
-    return (quotient+" notes of 100 Euro " +"\n"+ quotient2 + " notes of 50 Euro "  +"\n"+quotient3 +" notes of 20 Euro " +"\n"+quotient4 +" notes of 10 Euros ");
+    return (amt + " = " + quotient+" notes of 100 Euro " +"\n"+ quotient2 + " notes of 50 Euro "  +"\n"+quotient3 +" notes of 20 Euro " +"\n"+quotient4 +" notes of 10 Euros ");
 }
 
 document.write(atm(280));
