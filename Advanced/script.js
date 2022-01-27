4b274857b4fa5b4f2b1d75263006c2cb91b64c53
KateKatze <dr.katekogler@gmail.com>
2022-01-27 11:48:05 +0100

    old and new writing added

--- Advanced/script.js
+++ Advanced/script.js
@@ -5,3 +5,11 @@ function timeConv (min) {
 }
 
 console.log(timeConv(186));
+
+// function timeConv (min) {
+//     hrs = parseInt(min /60);
+//     rest=min%60;
+//     return (`${min} minutes is ${hrs} hour(s) and ${rest} minutes.`);
+// }
+
+// console.log(timeConv(186));
