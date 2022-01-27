9e9051e432c9c71ed6702e1214f83e76e4f4114c
AndreasPlangger <andreas_plangger@hotmail.com>
2022-01-27 10:49:41 +0100

    add folder

---
+++ Andreas/ex_1/script.js
@@ -0,0 +1,5 @@
+function crystalGazer(job_title,location,partners_name,number_childrens){
+    return(" You will be a " +job_title+ " in " +location+ " and married to " +partners_name+ " with " +number_childrens+ " children.")
+}
+
+console.log(crystalGazer("painter","Paris","Michelle",6));
